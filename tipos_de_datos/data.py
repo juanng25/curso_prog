2188c83b455f77f49be74ce54a9c73b4a3587949
Juan <jmng3025@gmail.com>
2023-11-15 13:01:46 -0500

    tipos de datos

---
+++ tipos_de_datos/data.py
@@ -0,0 +1,31 @@
+textos= 'hola'
+entero= 5
+decimal= 1.00
+booleano= True
+lista= ['a','b','c']
+
+nombre= 'Julissa'
+nombre= 'Juan'
+print(nombre)
+
+print(type(decimal))
+ano= 2023
+nacimiento= 1997
+edad= ano - nacimiento
+print(edad)
+
+print('mi nombre es: '+ nombre+ ' y tengo '+ str(edad) +' anos')
+
+verdad= 5<10
+print(verdad)
+mentira= 10<5
+print(mentira)
+
+animes= ['hunterxhunter','guiltycrown','fullmetal']
+print(animes)
+animes.append('atackoftitan')#agregar datos
+print(animes)
+animes[2]= 'haykiu'#editar datos
+print(animes)
+animes.pop(1)#eliminar datos
+print(animes)
